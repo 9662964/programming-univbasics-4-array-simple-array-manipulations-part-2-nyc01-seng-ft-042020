97f2eaf7f85544df31383da3afe726e3083c47fe
ILJOO CHAE <9662964@gmail.com>
2020-04-14 01:58:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,34 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+  return array1
+  
+end
+
+
+def using_insert(array,ele1)
+  array.insert(4,ele1)
+  return array
+  
+end
+
+def using_uniq(array)
+  array.uniq
+  return array
+end
+
+
+def using_flatten(array)
+  array.flatten()
+  return array
+end
+  
+  
+def using_delete(array,ele1)
+  array.delete(ele1)
+end
+
+
+def using_delete_at(array,ele1)
+  array.delete_at(ele1)
+end
+
